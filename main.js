5df2ca299edf2185df20cf6b89447e2a334d022f
lisbajramii <onlb.lis0@gmail.com>
2025-02-06 19:02:27 +0100

    afasfas
    saf

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ let friendsNames = ["filani1", "filani2", "filani3", "filani4", "filani5"];
 
 
 console.log(friendsNames[1]);
+
+
+let numbers = [10, 20, 30, 40, 50];
+
+
+numbers.push(60);
+
+
+numbers.shift();
+
+
+numbers.splice(1, 0, 15);
+
+console.log(numbers);
